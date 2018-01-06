359263b8e0e4029337e2b034020944222b783dfb
Yeh <Jamie.Yeh@disney.com>
2018-01-06 14:36:14 -0800

    created an array for cards

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+
+var cards = ["queen", "queen", "king", "king"];
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardThree);
