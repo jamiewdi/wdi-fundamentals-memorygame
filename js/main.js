342b91c6d02e0d53ad64bfd0024da24b62b6b6c7
Yeh <Jamie.Yeh@disney.com>
2018-01-06 17:23:39 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,19 +2,20 @@
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[2]) {
-    alert("You found a match!");
-  } else {
+var checkForMatch = function() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
+    } else {
     alert("Sorry, try again.");
-  }
+    }
 }
+
+var flipCard = function(cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
